28e44a5e1165c1fb223a4c09ce85a5515c2beca3
kraft <kaungmyat.mindfulness@gmail.com>
2025-04-09 02:54:13 +0700

    feat: logger enhancement

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { AuthModule } from 'src/auth/auth.module';
+import { CategoryModule } from 'src/category/category.module';
 import { UnusedImageCleanupService } from 'src/common/cleanup/unused-image-cleanup.service';
 import { CommonModule } from 'src/common/common.module';
+import { LoggerMiddleware } from 'src/common/middleware/logger.middleware';
 import { EmailModule } from 'src/email/email.module';
 import { MenuModule } from 'src/menu/menu.module';
 import { OrderModule } from 'src/order/order.module';
@@ -10,14 +12,12 @@ import { UserModule } from 'src/user/user.module';
 
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
 import { ScheduleModule } from '@nestjs/schedule';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 
 import { AppService } from './app.service';
 import { PrismaService } from './prisma.service';
-import { APP_GUARD } from '@nestjs/core';
-import { CategoryModule } from 'src/category/category.module';
-import { LoggerMiddleware } from 'src/common/middleware/logger.middleware';
 
 @Module({
   imports: [
--- src/common/middleware/logger.middleware.ts
+++ src/common/middleware/logger.middleware.ts
@@ -6,18 +6,27 @@ export class LoggerMiddleware implements NestMiddleware {
   private readonly logger = new Logger('HTTP');
 
   use(request: Request, response: Response, next: NextFunction): void {
-    const { ip, method, originalUrl: url } = request;
-    const userAgent = request.get('user-agent') || '';
+    const startTime = Date.now();
+    const { ip, method, originalUrl } = request;
+    const userAgent = request.get('user-agent') || '-';
+
+    this.logger.log(`--> ${method} ${originalUrl} - ${userAgent} ${ip}`);
 
     response.on('finish', () => {
+      const endTime = Date.now();
+      const duration = endTime - startTime;
       const { statusCode } = response;
-      const contentLength = response.get('content-length');
+      const contentLength = response.get('content-length') || '-';
 
-      this.logger.verbose(`Request Body: ${JSON.stringify(request.body)}`);
+      const message = `<-- ${method} ${originalUrl} ${statusCode} ${duration}ms - ${contentLength} bytes`;
 
-      this.logger.log(
-        `${method} ${url} ${statusCode} ${contentLength || '-'} - ${userAgent} ${ip}`,
-      );
+      if (statusCode >= 500) {
+        this.logger.error(message);
+      } else if (statusCode >= 400) {
+        this.logger.warn(message);
+      } else {
+        this.logger.log(message);
+      }
     });
 
     next();
